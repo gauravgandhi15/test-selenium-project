93c55bedbcf6c306bf37aa97a9938c06a4d21aa8
Gaurav Gandhi <gauravgandhi15@gmail.com>
2019-03-03 20:48:17 -0500

    Add files via upload

---
+++ test-selenium-maven-project/src/test/java/com/test/SampleTest.java
@@ -0,0 +1,23 @@
+package com.test;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+	@BeforeClass
+	public void setup() {
+
+	}
+
+	@Test
+	public void demoTest() {
+
+	}
+
+	@BeforeClass
+	public void tearDown() {
+
+	}
+
+}
