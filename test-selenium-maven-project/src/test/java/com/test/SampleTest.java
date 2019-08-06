e026c5ba099e6fd7f4c1e9c50c6989bb6f72ab1e
Gaurav Gandhi <gauravgandhi15@gmail.com>
2019-08-06 09:51:06 -0400

    updating SampleTest.java

--- test-selenium-maven-project/src/test/java/com/test/SampleTest.java
+++ test-selenium-maven-project/src/test/java/com/test/SampleTest.java
@@ -1,23 +1,36 @@
 package com.test;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class SampleTest {
 
-	@BeforeClass
-	public void setup() {
+    String baseURL = "https://www.google.com";
 
-	}
 
-	@Test
-	public void demoTest() {
+    @BeforeClass
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "/Users/ggand5/Downloads/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get(baseURL);
+        try {
+            // thread to sleep for 1000 milliseconds
+            Thread.sleep(5000);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 
-	}
+    @Test
+    public void demoTest() {
 
-	@BeforeClass
-	public void tearDown() {
+    }
 
-	}
+    @BeforeClass
+    public void tearDown() {
+
+    }
 
 }
